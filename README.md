d1f76aa98324bb58ea32e2a7705abd0c1061de42
Rudolf Bargholz <rudolf@bargholz.ch>
2017-04-25 11:42:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,97 @@
+# ubuntu-elixir-elm-install-shell-script
+Shell script that automates install of Elixir, Erlang, Node 7.x, Postgres, pgadmin, Phoenix, Elm, Atom, Sublime Text 3 (dev build).
+
+# Usage
+
+## Warning
+
+**This script is designed for initial installation on a clean system. It is not designed to recognize if software has already been installed or system settings have already been changed.**
+
+In some cases it does not matter if the script is executed on a system with software installed (most of the `apt-get install` ought to be safe), but the Postgres install might overwrite existing files or overwrite the password of the postgres user.
+
+## Installation
+
+- Copy the `install.sh` to the Desktop, for example.
+
+- Assign the file `install.sh` the permission `Allow executing file as program`.
+
+- Open the file with a text editor, search for `GIT_USER_EMAIL`, `GIT_USER_NAME`, `POSTGRES_APP_DB_USER` and `POSTGRES_APP_DB_PASS`, enter the appropriate values and save the changes.
+
+- Open a terminal window
+```Shell
+cd Desktop
+./install.sh 2>&1 | tee install.log
+```
+
+You will be required to enter your superuser password when the first `sudo` is referenced in the script.
+
+The `... 2>&1 | tee ...` redirects both stdout and stderr to a file and the stdout. You can follow the installation in the terminal, but the terminal output is also saved to the file `install.log` in the same folder as the `install.sh`.
+
+Do _**not**_ start the script as follows:
+
+```Shell
+sudo ./install.sh
+```
+
+This will start the entire script as a superuser, and any downloaded files will have superuser permissions. The current user will not have permissions for these files and parts of the installation will fail.
+
+The script uses [asdf](https://github.com/asdf-vm/asdf) (https://github.com/asdf-vm/asdf) to install Erlang, Elixir and Elm. The tool `asdf` allows one to install plugins, that then allow the user to install more than one version of the package. One can then use asdf to specify which version should be used. Each plugin may specify prerequisites. The plugins load the application via github, and then build the software. In the case of Erlang this can take a while.
+
+After installing asdf, to install Erlang:
+
+```Shell
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+```
+
+This will return a list of available versions of Erlang:
+
+```Shell
+asdf list-all erlang
+```
+
+This will install Erlang 19.3:
+
+```Shell
+asdf install erlang 19.3
+```
+
+This will use Erlang version 19.3:
+
+```Shell
+asdf global erlang 19.3
+asdf local erlang 19.3
+```
+
+# Useful commands
+
+#### List the locally installed packages
+
+``````Shell
+sudo dpkg -l
+```
+
+#### List the locally installed packages and find some packages
+
+``````Shell
+sudo dpkg -l | grep erlang
+```
+
+#### Get top level global npm packages
+
+``````Shell
+npm -g list --depth=0
+```
+
+### When were which packages installed
+
+```Shell
+subl /var/log/apt/history.log
+```
+
+### Open psql with the user postgres
+
+``````Shell
+psql -U postgres -W -h localhost
+```
+
+You will need to enter the password `postgres`
---
+++ install.sh
@@ -0,0 +1,386 @@
+#!/bin/bash
+
+# Usage
+# cd Desktop
+# ./install.sh 2>&1 | tee install.log
+#
+
+
+# https://github.com/asdf-vm/asdf
+
+main_section_heading () {
+    echo " "
+    echo " "
+    echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
+    echo " $1"
+    if [ -n "$2" ]; then
+        # $2 is set to a non-empty string
+        echo " $2"
+    fi
+    echo "======================================================================"
+}
+
+sub_section_heading () {
+    echo " "
+    echo " "
+    echo "--- $1"
+    echo "--------------------"
+}
+# Personal notes ...
+# System settings: Language Support
+# Install new language: German
+# Set Regional Format to "Deutsch (Schweiz)". Apply System-Wide
+# System settings: Text Entry
+# Add new Input Source: German (Switzerland)
+# sudo apt-get -y install open-vm-tools open-vm-tools-desktop net-tools gnome-shell git
+
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "Environment variables"
+# ----------------------------------------------------------------------------------------------------
+# Env variables used in the commands below
+
+sub_section_heading "postgres"
+set -x
+# Edit the following to change the name of the database user that will be created:
+POSTGRES_APP_DB_USER=postgres
+POSTGRES_APP_DB_PASS=$POSTGRES_APP_DB_USER
+set +x
+
+sub_section_heading "git"
+set -x
+GIT_USER_EMAIL="" # Enter your email address here
+GIT_USER_NAME="" # Enter your name here
+GIT_CREDENTIAL_HELPER_CACHE_TIMEOUT="86400"
+set +x
+
+
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "General dependencies"
+# ----------------------------------------------------------------------------------------------------
+set -x
+
+cd ~/Downloads/
+mkdir autoinstall
+cd autoinstall
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# net-tools contains ifconfig
+sudo apt-get -y install net-tools
+
+# git is needed by a lot of tools below
+sudo apt-get -y install git
+
+# For atom
+sudo add-apt-repository -y ppa:webupd8team/atom
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# libncurses5-dev
+sudo apt-get -y install \
+autoconf \
+automake \
+bison \
+build-essential \
+libssl-dev \
+libssh-dev \
+libyaml-dev \
+libreadline-dev \
+zlib1g-dev \
+libncurses-dev \
+libffi-dev \
+libgdbm3 \
+libgdbm-dev \
+unison \
+unison-gtk \
+htop \
+inotify-tools \
+libxml2-utils \
+fop \
+xsltproc \
+libxslt1-dev \
+libtool \
+unixodbc-dev
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "git"
+# ----------------------------------------------------------------------------------------------------
+set -x
+if [ -n "${GIT_USER_EMAIL}" ]; then
+    # "VAR is set to a non-empty string"
+    git config --global user.email "$GIT_USER_EMAIL"
+fi
+if [ -n "${GIT_USER_EMAIL}" ]; then
+    git config --global user.name "$GIT_USER_NAME"
+fi
+
+if [ -n "${GIT_CREDENTIAL_HELPER_CACHE_TIMEOUT}" ]; then
+    # Save your password in memory and set the cache time in minutes manually:
+    git config --global credential.helper 'cache --timeout='$GIT_CREDENTIAL_HELPER_CACHE_TIMEOUT
+    # Set the cache to timeout after x secconds
+    # 86400 = 1 day
+    # default is 15 minutes
+fi
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "asdf"
+# ----------------------------------------------------------------------------------------------------
+set -x
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.3.0
+
+add_to_file_if_not_exists() {
+  LINESEARCH=$1
+  LINEREPLACE=$2
+  FILE=$3
+  grep -qF "$LINESEARCH" "$FILE" || echo -e $LINEREPLACE >> "$FILE"
+}
+
+# For asdf and Ubuntu or other linux distros
+add_to_file_if_not_exists '. $HOME/.asdf/asdf.sh' '\n. $HOME/.asdf/asdf.sh' ~/.bashrc
+add_to_file_if_not_exists '. $HOME/.asdf/completions/asdf.bash' '\n. $HOME/.asdf/completions/asdf.bash' ~/.bashrc
+
+#Reloads the .bashrc for asdf, without needing to log in and out again
+. ~/.bashrc
+# This does not seem to work, so perform these steps manually within this session
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "Erlang, Elixir and Elm"
+# ----------------------------------------------------------------------------------------------------
+set -x
+
+# Dependencies for elixir, erlang, elm
+sub_section_heading "Dependencies building elixir, erlang, elm using asdf"
+
+
+
+# Use asdf to install Erlang, Elixir and Elm
+sub_section_heading "Add asdf plugins ..."
+
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+asdf plugin-add elm https://github.com/vic/asdf-elm.git
+asdf plugin-update --all
+
+asdf_install () {
+  APPNAME=$1
+  # Get the last line in the version number string (sed -e '$!d'). This will be the latest version available for the application.
+  LATEST_VERSION=$(asdf list-all $APPNAME | sed -e '$!d')
+  echo $APPNAME: $LATEST_VERSION
+  asdf install $APPNAME $LATEST_VERSION
+  asdf global $APPNAME $LATEST_VERSION
+  asdf local $APPNAME $LATEST_VERSION
+}
+
+sub_section_heading "Install ..."
+
+sub_section_heading "asdf_install erlang"
+asdf_install "erlang"
+
+sub_section_heading "asdf_install elixir"
+asdf_install "elixir"
+
+sub_section_heading "asdf_install elm"
+asdf_install "elm"
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "nodejs 7.x"
+# ----------------------------------------------------------------------------------------------------
+set -x
+
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt install -y nodejs
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "Additional tools that need npm"
+# ----------------------------------------------------------------------------------------------------
+set -x
+
+sub_section_heading "elm-oracle ..."
+sudo npm install -g elm-oracle
+
+sub_section_heading "elm-format ..."
+sudo npm install -g elm-format
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "Phoenix (Elixir)"
+# ----------------------------------------------------------------------------------------------------
+set -x
+
+mix local.hex --force
+mix local.rebar --force
+mix archive.install "https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez" --force
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "Postgres"
+# ----------------------------------------------------------------------------------------------------
+set -x
+
+sudo apt-get -y install postgresql postgresql-contrib libpq-dev pgadmin3
+
+PGVERSION=$(ls /etc/postgresql)
+PG_CONF="/etc/postgresql/$PGVERSION/main/postgresql.conf"
+PG_HBA="/etc/postgresql/$PGVERSION/main/pg_hba.conf"
+PG_DIR="/var/lib/postgresql/$PGVERSION/main"
+# this has been set at the top of the script POSTGRES_APP_DB_USER=postgres
+# this has been set at the top of the script POSTGRES_APP_DB_PASS=postgres
+
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
+
+if sudo cat "$PG_HBA" |
+  grep -xqFe "host    all             all             all                     md5"
+then
+  echo host    all             all             all                     md5 already exists
+else
+  echo not found
+  echo 'host    all             all             all                     md5' | sudo tee -a $PG_HBA
+fi
+
+if sudo cat "$PG_CONF" |
+  grep -xqFe "client_encoding = utf8"
+then
+  echo client_encoding = utf8 already exists
+else
+  echo not found
+  echo 'client_encoding = utf8' | sudo tee -a $PG_CONF
+fi
+
+sudo service postgresql restart
+
+sudo -u postgres bash -c "psql -c \"CREATE USER $POSTGRES_APP_DB_USER WITH PASSWORD '$POSTGRES_APP_DB_PASS';\""
+sudo -u postgres bash -c "psql -c \"ALTER USER $POSTGRES_APP_DB_USER WITH PASSWORD '$POSTGRES_APP_DB_PASS';\""
+
+# psql -U postgres -W -h localhost
+# Enter the password 'postgres' to log on to psql
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "Atom"
+# ----------------------------------------------------------------------------------------------------
+set -x
+
+sudo apt-get -y install atom
+
+apm install \
+autocomplete-elixir \
+elm-format \
+file-icons \
+git-plus \
+html-to-elm \
+language-elixir \
+language-elm \
+language-lisp \
+linter \
+linter-elixirc \
+linter-xmllint \
+merge-conflicts \
+minimap \
+project-manager \
+refactor \
+regex-railroad-diagram \
+split-diff \
+tabs-to-spaces \
+trailing-spaces \
+xml-formatter \
+monokai
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "Sublime Text 3 (dev build)"
+# ----------------------------------------------------------------------------------------------------
+set -x
+
+# Note: this is a dev build and requires a valid serial number
+# I was not able to find an easy way to get the last version number of Sublime Text 3
+LATESTNUMBER=$(wget -O - https://www.sublimetext.com/3dev | grep -o 'href="https://download.sublimetext.com/Sublime Text Build [0-9][0-9][0-9][0-9].dmg"' | grep -o -E '[0-9]+')
+
+# wget --quiet https://download.sublimetext.com/sublime-text_build-3112_amd64.deb && sudo dpkg -i sublime-text_build-3112_amd64.deb
+wget --quiet https://download.sublimetext.com/sublime-text_build-${LATESTNUMBER}_amd64.deb && sudo dpkg -i sublime-text_build-${LATESTNUMBER}_amd64.deb
+
+
+cwd=$(pwd)
+
+mkdir -p ~/.config/sublime-text-3/Installed\ Packages
+cd ~/.config/sublime-text-3/Installed\ Packages/
+wget --quiet https://packagecontrol.io/Package%20Control.sublime-package
+
+mkdir -p ~/.config/sublime-text-3/Packages/User
+cd ~/.config/sublime-text-3/Packages/User/
+wget --quiet https://github.com/rudolfb/ubuntu-elixir-elm-install-shell-script/raw/master/sublime-text-3/Package%20Control.sublime-settings
+wget --quiet https://raw.githubusercontent.com/rudolfb/ubuntu-elixir-elm-install-shell-script/master/sublime-text-3/Terminal.sublime-settings
+
+# Revert back to previous directory
+cd $cwd
+
+# When you open Sublime Text the package control will automatically be initialized, and all packages in the sublime.settings will be downloaded and installed.
+subl --background &
+
+
+set +x
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "Check the version numbers to test the installations"
+# ----------------------------------------------------------------------------------------------------
+
+sub_section_heading "git"
+echo "git --version"
+git --version
+echo "git config --list"
+git config --list
+
+sub_section_heading "node"
+echo "node --version"
+node --version
+echo "npm --version"
+npm --version
+
+sub_section_heading "postgres"
+echo "psql --version"
+psql --version
+
+sub_section_heading "erlang"
+echo "asdf current erlang"
+asdf current erlang
+
+echo "asdf current elixir"
+asdf current elixir
+
+sub_section_heading "elixir"
+echo "elixir --version"
+elixir --version
+
+sub_section_heading "elm"
+echo "asdf current elm"
+asdf current elm
+
+echo "elm --version"
+elm --version
+
+sub_section_heading "atom"
+echo "atom --version"
+atom --version
+echo "Enter 'atom' in a shell to start atom."
+
+# ----------------------------------------------------------------------------------------------------
+main_section_heading "End"
+# ----------------------------------------------------------------------------------------------------
+
+exec bash
