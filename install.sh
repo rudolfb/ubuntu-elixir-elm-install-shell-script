b3254f30534f737832e8c4af8389297d663abfa6
Rudolf Bargholz <rudolf@bargholz.ch>
2017-05-12 18:28:28 +0200

    Improved install/update of Sublime TExt 3 The newer version updates if ST3 is already installed, not overwriting installed packages

--- install.sh
+++ install.sh
@@ -313,27 +313,83 @@ main_section_heading "Sublime Text 3 (dev build)"
 # ----------------------------------------------------------------------------------------------------
 set -x
 
-# Note: this is a dev build and requires a valid serial number
-# I was not able to find an easy way to get the last version number of Sublime Text 3
-LATESTNUMBER=$(wget -O - https://www.sublimetext.com/3dev | grep -o 'href="https://download.sublimetext.com/Sublime Text Build [0-9][0-9][0-9][0-9].dmg"' | grep -o -E '[0-9]+')
+# Note: this is uses dev build and requires a valid serial number
 
-# wget --quiet https://download.sublimetext.com/sublime-text_build-3112_amd64.deb && sudo dpkg -i sublime-text_build-3112_amd64.deb
-wget --quiet https://download.sublimetext.com/sublime-text_build-${LATESTNUMBER}_amd64.deb && sudo dpkg -i sublime-text_build-${LATESTNUMBER}_amd64.deb
+# This will install or update sublime text, recognizing if the version number of the locally
+# installed ST3 is older than the remote version available. There will only be an update
+# if the remote version is larger than the local version.
+# If this is an initial install, the standard ST3 packages will be installed.
+arch=amd64
+channel=dev
 
+echo --------------------
 
-cwd=$(pwd)
+if [ "$channel" = "dev" ]
+then
+  remoteversionurl="http://www.sublimetext.com/updates/3/dev/updatecheck?platform=linux&arch=x64"
+else
+  remoteversionurl="http://www.sublimetext.com/updates/3/stable/updatecheck?platform=linux&arch=x64"
+fi
 
-mkdir -p ~/.config/sublime-text-3/Installed\ Packages
-cd ~/.config/sublime-text-3/Installed\ Packages/
-wget --quiet https://packagecontrol.io/Package%20Control.sublime-package
+localversion=$(dpkg -s sublime-text | grep Version  | grep -o -E '[0-9]+')
 
-mkdir -p ~/.config/sublime-text-3/Packages/User
-cd ~/.config/sublime-text-3/Packages/User/
-wget --quiet https://github.com/rudolfb/ubuntu-elixir-elm-install-shell-script/raw/master/sublime-text-3/Package%20Control.sublime-settings
-wget --quiet https://github.com/rudolfb/ubuntu-elixir-elm-install-shell-script/raw/master/sublime-text-3/Terminal.sublime-settings
+json=$(wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 20 -O - ${remoteversionurl})
+remoteversion=$(echo ${json} | grep -Po '"latest_version": \K[0-9]+')
 
-# Revert back to previous directory
-cd $cwd
+# remoteversion=$(wget -O - https://www.sublimetext.com/3${channel} | grep -o 'href="https://download.sublimetext.com/Sublime Text Build [0-9][0-9][0-9][0-9].dmg"' | grep -o -E '[0-9]+')
+
+remoteurl=https://download.sublimetext.com/sublime-text_build-${remoteversion}_${arch}.deb
+filename=sublime-text_build-${remoteversion}_${arch}.deb
+
+echo --------------------
+
+echo version json=${json}
+
+echo --------------------
+echo ${cwd}
+echo channel=${channel} 
+echo localversion=${localversion}
+echo remoteversion=${remoteversion}
+echo remoteurl=${remoteurl}
+echo remoteversionurl=${remoteversionurl}
+echo filename=${filename}
+echo --------------------
+
+if [ -z "$remoteversion" ]
+then
+  echo 'remoteversion variable is undefined'
+  exit 1
+fi
+
+if [[ "$localversion" != "$remoteversion" ]]
+then
+  echo Update Sublime Text 3 from ""${localversion}"" to ""${remoteversion}""
+  wget --quiet ${remoteurl} && sudo dpkg -i ${filename}
+  rm ${filename}
+
+  if [ -z "$localversion" ]
+  then
+    echo Install my Sublime Text 3 packages ...
+    cwd=$(pwd)
+
+    mkdir -p ~/.config/sublime-text-3/Installed\ Packages
+    cd ~/.config/sublime-text-3/Installed\ Packages/
+    wget --quiet https://packagecontrol.io/Package%20Control.sublime-package
+
+    mkdir -p ~/.config/sublime-text-3/Packages/User
+    cd ~/.config/sublime-text-3/Packages/User/
+    wget --quiet https://github.com/rudolfb/ubuntu-elixir-elm-install-shell-script/raw/master/sublime-text-3/Package%20Control.sublime-settings
+    wget --quiet https://github.com/rudolfb/ubuntu-elixir-elm-install-shell-script/raw/master/sublime-text-3/Terminal.sublime-settings
+
+    # Revert back to previous directory
+    cd $cwd
+  fi
+else
+  echo Sublime Text 3 local version ${localversion} is up to date  
+fi
+
+echo --------------------
+
 
 # When you open Sublime Text the package control will automatically be initialized, and all packages in the sublime.settings will be downloaded and installed.
 subl --background &
